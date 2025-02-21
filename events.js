d0954c9cc14bd0a6ddf147d3c184ce1a9752bd48
Administrator <murillowebdesigner@gmail.com>
2025-02-21 13:13:20 -0300

    Test Calendar

---
+++ events.js
@@ -0,0 +1,38 @@
+const events = [
+    {
+        "eventName": "Ass",
+        "eventUnit": "Escritório Central",
+        "description": "Discussão sobre metas do mês e novas estratégias.",
+        "date": "2025-02-25",
+        "arrivalTime": "09:00",
+        "checkIn": "08:45",
+        "checkOut": "11:00",
+        "carInfo": "Toyota Corolla - Preto",
+        "numberOfPeople": 5,
+        "numberOfChildren": 0
+    },
+    {
+        "eventName": "Ass",
+        "eventUnit": "Escritório Central",
+        "description": "Discussão sobre metas do mês e novas estratégias.",
+        "date": "2025-02-25",
+        "arrivalTime": "09:00",
+        "checkIn": "08:45",
+        "checkOut": "11:00",
+        "carInfo": "Toyota Corolla - Preto",
+        "numberOfPeople": 5,
+        "numberOfChildren": 0
+    },
+    {
+        "eventName": "Treinamento de Segurança",
+        "eventUnit": "Unidade Industrial",
+        "description": "Treinamento obrigatório sobre medidas de segurança no trabalho.",
+        "date": "2025-02-28",
+        "arrivalTime": "14:00",
+        "checkIn": "13:50",
+        "checkOut": "16:30",
+        "carInfo": "Van da Empresa",
+        "numberOfPeople": 12,
+        "numberOfChildren": 0
+    }
+];
